3c1bbbc823fffaa7b2d582ed8f7f790293f1f5ab
Delgado <rxd718@miami.edu>
2022-12-27 14:02:58 -0500

    added broken javascript to fix

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const start = document.getElementById("startButton");
+
+start.addEventListener("click", startGame) {
+    function startGame() {
+        if (i=75; i > 0; i--;) {
+            timerLeft = i + " second(s) remaining!";
+        }
+
+    }        
+
+}
