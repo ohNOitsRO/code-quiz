89b0dee0c6fbbee4baf39c2ce74eb203658b30c7
Delgado <rxd718@miami.edu>
2022-12-27 14:17:12 -0500

    added javascript

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 const start = document.getElementById("startButton");
+let timerLeft = 75;
 
 start.addEventListener("click", startGame) {
     function startGame() {
-        if (i=75; i > 0; i--;) {
-            timerLeft = i + " second(s) remaining!";
+        if (timerLeft; timerLeft > 0; timerLeft--;) {
+            timerLeft = timerLeft + " second(s) remaining!";
         }
 
     }        
