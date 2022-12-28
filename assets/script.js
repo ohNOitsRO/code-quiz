be0c12eb8b32d04e7d6310f2c6f2b6954460ac0d
Delgado <rxd718@miami.edu>
2022-12-28 11:27:48 -0500

    added countdown timer

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,25 @@
-const start = document.getElementById("startButton");
+var startGame = document.querySelector("#startButton");
+var firstAnswer = document.querySelector("#answer1");
+var secondAnswer = document.querySelector("#answer2");
+var thirdAnswer = document.querySelector("#answer3");
+var fourthAnswer = document.querySelector("#answer4");
 let timerLeft = 75;
 
-start.addEventListener("click", startGame) {
-    function startGame() {
-        if (timerLeft; timerLeft > 0; timerLeft--;) {
-            timerLeft = timerLeft + " second(s) remaining!";
-        }
+function countdown(){
+    var timerInterval = setInterval(function() {
+            timerLeft--;
+            startGame.textContent = timerLeft + " seconds remaining!";
+    
+    }, 1000);
 
-    }        
-
+    if (timerLeft === 0){
+        clearInterval(timerInterval);
+        
+    }
 }
+
+startGame.addEventListener("click", function() {
+        startGame.setAttribute("style", "box-shadow: 0px 0px;");
+        countdown();
+
+    });
