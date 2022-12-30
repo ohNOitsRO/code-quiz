cf7776b12e884fae145f53c0d9265587da58165d
Delgado <rxd718@miami.edu>
2022-12-30 10:23:37 -0500

    added questions logic

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var startGame = document.querySelector("#startButton");
+var questionBox = document.querySelector("#questions");
 var firstAnswer = document.querySelector("#answer1");
 var secondAnswer = document.querySelector("#answer2");
 var thirdAnswer = document.querySelector("#answer3");
@@ -9,7 +10,7 @@ function countdown(){
     var timerInterval = setInterval(function() {
         timerLeft--;
         startGame.textContent = timerLeft + " seconds remaining!";
-            
+        
         if (timerLeft === 0) {
             clearInterval(timerInterval);
             startGame.textContent = "Game Over!";
@@ -28,3 +29,58 @@ startGame.addEventListener("click", function() {
 
     });
 
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts',
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within ____.',
+        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses',
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store ____.',
+        choices: [
+            'numbers and strings',
+            'other arrays',
+            'booleans',
+            'all of the above',
+          ],
+        answer: 'all of the above',
+    },
+    {
+        title:
+            'String values must be enclosed within ____ when being assigned to variables.',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes',
+    },
+    {
+        title:
+            'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+        answer: 'console.log',
+    },
+    ];
+
+
+
+let lastQuestion = questions.length - 1;
+let currentQuestion = 0;
+
+
+function showQuestions(){
+    let q = questions[lastQuestion];
+    questionBox.textContent = q.title;
+    firstAnswer.textContent = q.choices[0];
+    secondAnswer.textContent = q.choices[1];
+    thirdAnswer.textContent = q.choices[2];
+    fourthAnswer.textContent = q.choices[3];
+}
+
+// when question is answered, show next question
+  currentQuestion++;
+
+  
+showQuestions();
